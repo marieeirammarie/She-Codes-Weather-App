52b5f202e8a6cd696908758f82f6b736d06b58d5
marieeirammarie <100724067+marieeirammarie@users.noreply.github.com>
2022-06-25 15:21:31 +0200

    Included my index.html design + Java script load date & time of users geographical location

---
+++ src/index.js
@@ -0,0 +1,43 @@
+// Function 1: Load current Date & Time
+function loadDate(now) {
+  let date = now.getDate();
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[now.getMonth()];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[now.getDay()];
+  let hours = now.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} –  ${date} ${month} –  ${hours} : ${minutes}`;
+}
+
+// Alert Function 1: Load current Date & Time
+let currentTime = document.querySelector("#current-time");
+let now = new Date();
+currentTime.innerHTML = loadDate(now);
