753d55c44d7ecf50351f1b5f356de0d6cdcb35f5
marieeirammarie <100724067+marieeirammarie@users.noreply.github.com>
2022-06-27 14:52:15 +0200

    added an id to my forecastimage

--- src/index.js
+++ src/index.js
@@ -130,7 +130,7 @@ function displayForecast(response) {
           <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
           <img src="http://openweathermap.org/img/wn/${
             forecastDay.weather[0].icon
-          }@2x.png" width="80" />
+          }@2x.png" width="80" id="forecast-image"/>
           <div class="weather-forecast-temperature">
             <span class="weather-forecast-temperature-max">${Math.round(
               forecastDay.temp.max
